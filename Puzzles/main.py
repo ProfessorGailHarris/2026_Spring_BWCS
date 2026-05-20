e2f8debfec9afa6ae5f89db57cb1239ebb13e75b
Gail Harris <gail.e.harris@gmail.com>
2026-05-20 11:09:21 -0700

    2026 Spring BWCS - Puzzles

---
+++ Puzzles/main.py
@@ -0,0 +1,53 @@
+# This is a comment line. It is ignored when the program is run.
+
+# Puzzles for Spring 2026 BWCS
+
+# INSTRUCTIONS
+# The puzzles are organized into sections. Each section begins with one or two lines like this:
+# 
+# PUZZLES about XXXX
+# HINT: here's some extra information that may help you solve the puzzles in this section.
+#
+#
+# To try each puzzle remove the "#" at the beginning of each statement in a puzzle section.
+# Run the project, which will cause error messages.
+# Figure out how to fix the code in the section so that no more errors appear when you run the project.
+# If you are stuck you should try a few things, and then ask for help if you are still stuck.
+# Remember, you can always put the "#" at the front of a line until you can receive help.
+# 
+
+# PUZZLES about OUTPUT
+# HINT: read the error messages carefully.
+
+#print "My name is long."
+
+#name = "Mr. Hayes"
+#print( "My teacher's name is" name)
+
+# PUZZLES about INPUT
+# HINT: input is treated as a string by default. You need to do something extra to use numerical input in a calculation.
+
+#temp = input(What is the water temperature? )
+#print ("Temp is", temp)
+
+#airTemp = flot(input("what is the air temperature in Celsius? "))
+#print("The temp in C is", airTemp, "and the temp in F is", airTemp * 9/5 + 32)
+
+#bodyTempFahrenheit = input("what is your body temperature in Fahrenheit? ")
+#bodyTempCelsius = (bodyTempFahrenheit - 32) * 5/9
+#print ("Your body temperature is", bodyTempCelsius, "Celsius")
+
+# PUZZLES about VARIABLES
+# HINT: explore the rules for variable names. Many computer languages have similar rules.
+
+#age = 14
+#print("my age is", AGE)
+
+#?averageAge = 14.5
+#average-Age = 13.5
+
+# PUZZLES about IF statements
+
+
+# PUZZLES about LOOPS
+
